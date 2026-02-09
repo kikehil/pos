556c38ac1fb9ed22ea1bb56e0641725acc738d25
Kike Hil <kikehil@gmail.com>
2026-02-09 11:46:00 -0600

    Rename src/prisma to src/database and add tsconfig.build.json

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './database/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { SalesModule } from './sales/sales.module';
 import { TenantsModule } from './tenants/tenants.module';
